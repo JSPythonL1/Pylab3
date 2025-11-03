ba491d14d80390915d7e3a3e7f118f65dbb478ce
Dawid <dk@dk.dk>
2025-11-03 09:36:20 +0100

    poprawa

--- tasks/task3.py
+++ tasks/task3.py
@@ -1,9 +1,15 @@
+from array import array
+from pathlib import Path
 import random
 
-def save_random_array(filename):
+def save_random_array(directory: str, filename: str = "result.txt"):
     """
     Generuje 10 liczb z przedziału (-5,5) i zapisuje je do pliku.
     Zwraca listę tych liczb.
+
+    :param directory: Ścieżka do katalogu, w którym ma być zapisany plik.
+    :param filename: Nazwa pliku wynikowego (domyślnie 'result.txt').
+    :return: Tablica (array) z wygenerowanymi liczbami.
     """
     # TODO: zaimplementuj funkcję
     pass
--- tasks/task5.py
+++ tasks/task5.py
@@ -1,6 +1,6 @@
-def histogram(filename):
+def histogram(input_path, output_path):
     """
-    Zwraca histogram znaków z pliku tekstowego.
+    Zwraca histogram znaków z pliku tekstowego i zapisuje je do pliku każda litera w nowej linii
     """
     # TODO: zaimplementuj funkcję
     pass
